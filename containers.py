e275e5cb185b09220db009dd26fc79ad12456f5c
Sangkon, Han <sigmadream@gmail.com>
2024-07-17 01:14:04 +0900

    Note  관련 코드 추가

--- containers.py
+++ containers.py
@@ -1,11 +1,18 @@
 from dependency_injector import containers, providers
 
+from note.application.note_service import NoteService
 from user.application.user_service import UserService
 from user.infra.repository.user_repo import UserRepository
+from note.infra.repository.note_repo import NoteRepository
 
 
 class Container(containers.DeclarativeContainer):
-    wiring_config = containers.WiringConfiguration(packages=["user"], )
+    wiring_config = containers.WiringConfiguration(packages=[
+        "user",
+        "note",
+    ], )
 
     user_repo = providers.Factory(UserRepository)
     user_service = providers.Factory(UserService, user_repo=user_repo)
+    note_repo = providers.Factory(NoteRepository)
+    note_service = providers.Factory(NoteService, note_repo=note_repo)
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.responses import JSONResponse
 
 from user.interface.controllers.user_controller import router as user_routers
 from user.interface.controllers.env_controller import router as env_routers
+from note.interface.controllers.note_controller import router as note_routers
 from containers import Container
 from sync_ex import router as sync_ex_routers
 from async_ex import router as async_ex_routers
@@ -15,6 +16,8 @@ app = FastAPI()
 app.container = Container()
 app.include_router(user_routers)
 app.include_router(env_routers)
+app.include_router(note_routers)
+
 app.include_router(sync_ex_routers)
 app.include_router(async_ex_routers)
 
